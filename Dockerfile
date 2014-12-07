f116bd2a6229155bdfcdf077da6ff5e8e351ef4f
Sergio Vera <sergio.vera@gmail.com>
2014-12-07 15:41:07 +0100

    Added dependencies and fixed volume mounting

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM php:5.4-cli
+RUN curl -sS https://getcomposer.org/installer | php
+RUN mv composer.phar /usr/local/bin/composer
+RUN pecl install mongo
+RUN pecl install zip
+RUN echo "extension=mongo.so\nextension=zip.so" > /usr/local/lib/php.ini
 ADD . /code
+WORKDIR /code
+RUN composer install
--- app/index.php
+++ app/index.php
@@ -1,11 +1,24 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Neutron\Silex\Provider\MongoDBODMServiceProvider;
+
 $app = new Silex\Application();
+$app->register(new MongoDBODMServiceProvider(), array(
+    'doctrine.odm.mongodb.connection_options' => array(
+        'database' => 'MONGODB_DB',
+        'host'     => 'mongo',
+        'options'  => array('fsync' => false)
+    ),
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 $app['debug'] = true;
 
-$app->get('/', function() {
-    return "Allright";
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.twig', array(
+    ));
 });
 
 $app->run();
