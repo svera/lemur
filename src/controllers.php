e2c1dce57a45ccc9a431e8d244a867b2651d044d
Sergio Vera <severa@ebay.com>
2014-12-09 17:47:13 +0100

    Made separate actions for post and get

--- src/controllers.php
+++ src/controllers.php
@@ -1,17 +1,28 @@
 <?php
 
-use Src\Models\Request;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Src\Models\PullRequest;
 
 $app->get('/', function() use ($app) {
-    $demo = new Request();
-    $demo->setName('Test');
-
-    $app['doctrine.odm.mongodb.dm']->persist($demo);
-    $app['doctrine.odm.mongodb.dm']->flush();
-    $demos = $app['doctrine.odm.mongodb.dm']
-    ->getRepository('Src\\Models\\Request')
+    $pullRequests = $app['doctrine.odm.mongodb.dm']
+    ->getRepository('Src\\Models\\PullRequest')
     ->findAll();
     return $app['twig']->render('index.twig', array(
-        'demos' => $demos
+        'pullRequests' => $pullRequests
     ));
+});
+
+$app->post('/', function(Request $request) use ($app) {
+    $payload = json_decode($request->get('payload'), true);
+    if ($payload['action'] == 'opened') {
+        $pullRequest = new PullRequest();
+        $pullRequest->setName($payload['pull_request']['title']);
+        $pullRequest->setCreatedBy($payload['pull_request']['user']['login']);
+        $pullRequest->setCreatedAt($payload['pull_request']['created_at']);
+        $pullRequest->setVcs('Github');
+        $app['doctrine.odm.mongodb.dm']->persist($pullRequest);
+        $app['doctrine.odm.mongodb.dm']->flush();
+        return new Response('Thank you!', 200);
+    }
 });
---
+++ src/models/PullRequest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Src\Models;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/** @ODM\Document */
+class PullRequest
+{
+    /** @ODM\Id */
+    private $id;
+
+    /** @ODM\String */
+    private $name;
+
+    /** @ODM\Int */
+    private $numberComments;
+
+    /** @ODM\Int */
+    private $numberApprovals;
+
+    /** @ODM\Int */
+    private $numberDisapprovals;
+
+    /** @ODM\String */
+    private $createdBy;
+
+    /** @ODM\Timestamp */
+    private $createdAt;
+
+    /** @ODM\String */
+    private $vcs;
+
+    public function __construct()
+    {
+        $this->numberComments = 0;
+        $this->numberApprovals = 0;
+        $this->numberDisapprovals = 0;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setNumberComments($number)
+    {
+        $this->numberComments = $number;
+    }
+
+    public function getNumberComments()
+    {
+        return $this->numberComments;
+    }
+
+    public function setNumberApprovals($number)
+    {
+        $this->numberApprovals = $number;
+    }
+
+    public function getNumberApprovals()
+    {
+        return $this->numberApprovals;
+    }
+
+    public function setNumberDisapprovals($number)
+    {
+        $this->numberDisapprovals = $number;
+    }
+
+    public function getNumberDisapprovals()
+    {
+        return $this->numberDisapprovals;
+    }
+
+    public function setCreatedBy($name)
+    {
+        $this->createdBy = $name;
+    }
+
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedAt($timestamp)
+    {
+        $this->createdAt = $timestamp;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setVcs($name)
+    {
+        $this->vcs = $name;
+    }
+
+    public function getVcs()
+    {
+        return $this->vcs;
+    }
+}
