ecce0fe5d6a36484f7cd083cdcf4155d26f14f53
Sergio Vera <sergio.vera@gmail.com>
2014-12-27 12:24:49 +0100

    Refactored tests, now 100% more DRY

---
+++ app/src/tests/ControllersTest.php
@@ -0,0 +1,111 @@
+<?php
+
+use Silex\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ControllersGithubTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        putenv('LEMUR_ENV=test');
+        $app = require __DIR__.'/../../web/index.php';
+        $app['session.test'] = true;
+        return $app;
+    }
+
+    public function testInitialPage()
+    {
+        $this->app['session']->set('access_token', 'fakeToken');
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testNonExistentUrl()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/nonexistent');
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @dataProvider noPayloadOrWrongPayloadProvider
+     */
+    public function testNoPayloadOrWrongPayloadSent($url, $payload)
+    {
+        $client = $this->createClient();
+        $client->request(
+            'POST',
+            $url,
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            $payload
+        );
+        $this->assertEquals(
+            Response::HTTP_BAD_REQUEST,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function noPayloadOrWrongPayloadProvider()
+    {
+        return [
+                  ['/github/pullRequest', ''],
+                  ['/gitlab/pullRequest', '']
+               ];
+    }
+
+    /**
+     * @dataProvider newAndUpdatePullRequestProvider
+     */
+    public function testNewAndUpdatePullRequest($url, $payload, $expected)
+    {
+        $client = $this->createClient();
+        $client->request(
+            'POST',
+            $url,
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            $payload
+        );
+        $this->assertEquals($expected, $client->getResponse()->getStatusCode());
+    }
+
+    public function newAndUpdatePullRequestProvider()
+    {
+        return [
+                  ['/github/pullRequest', file_get_contents(__DIR__.'/fixtures/githubNewPullRequestPayload.json'), Response::HTTP_CREATED],
+                  ['/github/pullRequestComment', file_get_contents(__DIR__.'/fixtures/githubNewPullRequestCommentPayload.json'), Response::HTTP_OK],
+                  ['/gitlab/pullRequest', file_get_contents(__DIR__.'/fixtures/gitlabNewMergeRequestPayload.json'), Response::HTTP_CREATED],
+                  ['/gitlab/pullRequestComment', '', Response::HTTP_BAD_REQUEST]
+               ];
+    }
+
+    /**
+     * @dataProvider closePullRequestProvider
+     */
+    public function testClosePullRequest($url, $payload)
+    {
+        $client = $this->createClient();
+        $client->request(
+            'POST',
+            $url,
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            $payload
+        );
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function closePullRequestProvider()
+    {
+        return [
+                  ['/github/pullRequest', file_get_contents(__DIR__.'/fixtures/githubClosePullRequestPayload.json')],
+                  ['/gitlab/pullRequest', file_get_contents(__DIR__.'/fixtures/gitlabCloseMergeRequestPayload.json')]
+               ];
+    }
+
+}
