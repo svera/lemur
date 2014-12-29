c221191297c5084080f67c2601b9ac55f5afeaa9
Sergio Vera <sergio.vera@gmail.com>
2014-12-29 10:43:15 +0100

    more tries

--- app/web/index.php
+++ app/web/index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
 
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../src/controllers.php';
